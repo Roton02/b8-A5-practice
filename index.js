1059b8bd2aa4d6709c92a46c33de4bab82244609
Roton02 <rph645102@gmail.com>
2024-02-16 10:05:13 +0600

    added js code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const card = document.querySelectorAll('.card');
 for (let i = 0; i < card.length; i++) {
     const element = card[i];
     // console.log(element);
-    element.addEventListener('click', function(){
+    element.addEventListener('click', function (){
         // console.log('clicked succesfully');
         // show product name 
         const cardProductName = element.querySelector('h3').innerText
@@ -26,17 +26,37 @@ for (let i = 0; i < card.length; i++) {
     })
 
 }
-document.getElementById('apply-btn').addEventListener('click', function(){
+document.getElementById('apply-btn').addEventListener('click', function (){
     const inputText = document.getElementById('input-field');
     const inputField = inputText.value.split(' ').join('').toUpperCase();
-    if(sum>=200){
-        if(inputField === SELL200){
+    if(sum >= 200){
+        if(inputField === 'SELL200'){
             const discount = document.getElementById('discountPrice');
-            discount.innerText = sum*0.2;
+            // const p = document.createElement('p');
+            // p.innerText = sum*0.2;
+            // discount.appendChild(p)
+           const discountAmount = sum*0.2;
+           discount.innerText = discountAmount.toFixed(2); 
+
+            const total = document.getElementById('total');
+            const setTotalPrice = sum - discountAmount.toFixed(2);
+            total.innerText = setTotalPrice;
+            inputText.value = " ";
+        }else{
+            alert("mammu thiktahk likho")
+            inputText.value = " ";
         }
     }else{
        alert('mammu minium 200$ bey korte hobe....!')
+       inputText.value = " ";
     }
-    console.log(inputField);
+    // console.log(inputField);
 })
+
+const getBey = document.getElementById('Btn-purcase');
+getBey.addEventListener('click',function(){
+    document.getElementById('title-container').innerText = " ";
+
+});
+
 
